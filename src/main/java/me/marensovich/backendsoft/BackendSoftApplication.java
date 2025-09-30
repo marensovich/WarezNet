23aec642d2287c915dd2980441fef425f0fa3255
marensovich <marensovich@mail.ru>
2025-10-01 00:37:21 +0300

    initial commit

---
+++ src/main/java/me/marensovich/backendsoft/BackendSoftApplication.java
@@ -0,0 +1,13 @@
+package me.marensovich.backendsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/marensovich/backendsoft/BackendSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package me.marensovich.backendsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
