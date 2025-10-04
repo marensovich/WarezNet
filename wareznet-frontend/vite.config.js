2774cb1c53f1edcd7115c30e36a769d1c8d86889
CALLlA1011666 <lexa2268spb@gmail.com>
2025-10-04 16:21:41 +0300

    FE_INIT

--- wareznet-frontend/vite.config.js
+++ wareznet-frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // <- Make sure to import path
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      __PUBLIC: path.resolve(__dirname, 'public'),
+      __SRC: path.resolve(__dirname, 'src'),
+      __FONTS: path.resolve(__dirname, 'public/fonts'), // <- Now points to the actual directory
+      __CSS_ASSETS: path.resolve(__dirname, 'src/CSS/ASSET_IMPORT')
+    }
+  }
 })
